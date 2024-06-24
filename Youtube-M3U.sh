c659c28b8d1bea42444c06fd96daebf98bf5ff8a
Nasim Aktar <67604361+Nasimaktar99@users.noreply.github.com>
2024-06-24 19:39:41 +0530

    Update and rename beinsports-haber.sh to Youtube-M3U.sh

--- Youtube-M3U.sh
+++ Youtube-M3U.sh
@@ -6,6 +6,6 @@ python3 -m pip install requests
 
 cd $(dirname $0)/scripts/
 
-python3 beinsports-haber.py > ../beinsports-haber.m3u8
+python3 Youtube-M3U.py > ../Youtube-M3U.m3u8
 
 echo m3u8 grabbed
